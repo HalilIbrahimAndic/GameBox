4b85b52c8d377e3cbd655607a6f67ed7d6545c88
Halil Ibrahim Andic <hiandic@gmail.com>
2023-01-22 00:57:07 +0300

    feature/notes: NoteCell created.

---
+++ GameBox/GameBox/Modules/Note/NoteCell.swift
@@ -0,0 +1,18 @@
+//
+//  NoteCell.swift
+//  GameBox
+//
+//  Created by Halil Ibrahim Andic on 22.01.2023.
+//
+
+import UIKit
+
+class NoteCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+    
+}
