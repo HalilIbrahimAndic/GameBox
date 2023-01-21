7c3c44c5c63c4f0a06ade71d3a3b73dee9df3018
Halil Ibrahim Andic <hiandic@gmail.com>
2023-01-22 01:10:14 +0300

    feature/notes: NoteCellModel created

--- GameBox/GameBox/Modules/Note/NoteCell.swift
+++ GameBox/GameBox/Modules/Note/NoteCell.swift
@@ -9,10 +9,20 @@ import UIKit
 
 class NoteCell: UITableViewCell {
 
+    @IBOutlet weak var noteTitle: UILabel!
+    @IBOutlet weak var noteDescription: UILabel!
+    @IBOutlet weak var noteDate: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-
     }
 
-    
+    func configure(with model: GameListCellModel) {
+        contentImageView.kf.setImage(with: URL.init(string: model.background_image))
+        gameNameLabel.text = model.name
+        releasedValueLabel.text = model.released
+        ratingLabel.text = "\(model.rating) / \(model.rating_top)"
+    }
 }
+
+
---
+++ GameBox/GameBox/Services/NoteModel.swift
@@ -0,0 +1,15 @@
+//
+//  NoteModel.swift
+//  GameBox
+//
+//  Created by Halil Ibrahim Andic on 22.01.2023.
+//
+
+import Foundation
+
+struct NoteModel {
+    let id: Int
+    let title: String
+    let description: String
+    let date: Date
+}
