e7b531b3a57d6c08612818773ffd25ba38e2c000
Halil Ibrahim Andic <hiandic@gmail.com>
2023-01-23 13:30:38 +0300

    feature: Local Notification is added.

--- GameBox/GameBox/App/AppDelegate.swift
+++ GameBox/GameBox/App/AppDelegate.swift
@@ -10,13 +10,12 @@ import CoreData
 import DropDown
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         DropDown.startListeningToKeyboard()
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.badge,.sound]) { granted, error in }
         return true
     }
 
--- GameBox/GameBox/Modules/NoteDetail/NoteDetailViewController.swift
+++ GameBox/GameBox/Modules/NoteDetail/NoteDetailViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class NoteDetailViewController: UIViewController {
+class NoteDetailViewController: UIViewController, UNUserNotificationCenterDelegate {
     
     @IBOutlet weak var gameNameField: UITextField!
     @IBOutlet weak var gameTextField: UITextField!
@@ -15,17 +15,22 @@ class NoteDetailViewController: UIViewController {
     
     private let viewModel = NoteDetailViewModel()
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
  
     @IBAction func saveButtonPressed(_ sender: Any) {
+        localNotification()
+        UNUserNotificationCenter.current().delegate = self
+        
         if gameNameField.text != "" && gameTextField.text != "" {
             let myNote = NoteCellModel(name: gameNameField.text!, note: gameTextField.text!, date: Date())
             viewModel.saveNote(myNote)
             
+            NotificationCenter.default.post(name: Notification.Name("NoteNotification"), object: nil)
             dismiss(animated: true, completion: nil)
-            //navigationController?.popViewController(animated: true)
         } else {
             showAlert()
         }
@@ -35,5 +40,23 @@ class NoteDetailViewController: UIViewController {
         let alertController = UIAlertController(title: "Empty Fields", message: "Please fill all fields before saving", preferredStyle: .alert)
         alertController.addAction(.init(title: "Ok", style: .default))
         present(alertController, animated: true)
+    }
+}
+
+extension NoteDetailViewController {
+    
+    func localNotification() {
+        NotificationCenter.default.addObserver(self, selector: #selector(newNoteSaved), name: Notification.Name("NoteNotification"), object: nil)
+    }
+    
+    @objc func newNoteSaved() {
+        let notificationManager: NotificationProtocol = LocalNotificationManager.shared
+        notificationManager.sendNotification(title: "New Note!", message: "Your note is saved and listed.")
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.sound, .banner, .badge, .list])
     }
 }
